4e46a49c9b08d2d23335c4d747bfa6d7cb38cbb2
mrtwisterzzz <73936892+mrtwisterzzz@users.noreply.github.com>
2023-04-14 18:18:40 +0300

    added iterator

--- src/main/java/org/example/Randoms.java
+++ src/main/java/org/example/Randoms.java
@@ -5,12 +5,12 @@ import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    protected PrimitiveIterator.OfInt randomIterator;
     protected int min, max;
+    protected Random random;
 
 
     public Randoms(int min, int max) {
-        randomIterator = new Random().ints(min, (max + 1)).iterator();
+        this.random = new Random();
         this.min = min;
         this.max = max;
 
@@ -26,7 +26,8 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                return randomIterator.nextInt();
+                int i = max - min + 1;
+                return random.nextInt(Math.abs(i)) + min;
             }
         };
     }
